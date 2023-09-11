b2ea633d69cfa7491e19ef8b22d57293c0483e29
tosha504 <tosha.rovniy@gmail.com>
2023-09-11 09:36:21 +0200

    search none

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template part for displaying a message that posts cannot be found
  *
@@ -10,42 +11,44 @@
 ?>
 
 <section class="no-results not-found">
-	<header class="page-header">
-		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'start' ); ?></h1>
-	</header><!-- .page-header -->
+	<div class="container">
+		<header class="page-header">
+			<h1 class="page-title"><?php esc_html_e('Nic nie znaleziono', 'start'); ?></h1>
+		</header><!-- .page-header -->
 
-	<div class="page-content">
-		<?php
-		if ( is_home() && current_user_can( 'publish_posts' ) ) :
+		<div class="page-content">
+			<?php
+			if (is_home() && current_user_can('publish_posts')) :
 
-			printf(
-				'<p>' . wp_kses(
-					/* translators: 1: link to WP admin new post page. */
-					__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'start' ),
-					array(
-						'a' => array(
-							'href' => array(),
-						),
-					)
-				) . '</p>',
-				esc_url( admin_url( 'post-new.php' ) )
-			);
+				printf(
+					'<p>' . wp_kses(
+						/* translators: 1: link to WP admin new post page. */
+						__('Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'start'),
+						array(
+							'a' => array(
+								'href' => array(),
+							),
+						)
+					) . '</p>',
+					esc_url(admin_url('post-new.php'))
+				);
 
-		elseif ( is_search() ) :
+			elseif (is_search()) :
 			?>
 
-			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'start' ); ?></p>
+				<p><?php esc_html_e('Przepraszamy, ale nic nie pasuje do wyszukiwanych haseł. Spróbuj ponownie z innymi słowami kluczowymi.', 'start'); ?></p>
 			<?php
-			get_search_form();
+				echo search_nav();
 
-		else :
+			else :
 			?>
 
-			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'start' ); ?></p>
+				<p><?php esc_html_e('It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'start'); ?></p>
 			<?php
-			get_search_form();
+				get_search_form();
 
-		endif;
-		?>
-	</div><!-- .page-content -->
+			endif;
+			?>
+		</div><!-- .page-content -->
+	</div>
 </section><!-- .no-results -->
