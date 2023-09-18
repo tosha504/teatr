8be79bfbd53741fe4a68b1eb8bff5b2ac8b5042b
tosha504 <tosha.rovniy@gmail.com>
2023-09-18 15:17:34 +0200

    clear

--- archive-performance.php
+++ archive-performance.php
@@ -17,12 +17,6 @@ if (isset($_GET['type']) && $_GET['type'] === 'out') {
   $parmas_array['type'] = 'out';
   $description =  get_field('performaces_out', 'options');
 }
-
-
-// if (isset($_GET['children']) && $_GET['children'] === 'yes') {
-//   $parmas_array['children'] = 'yes';
-// }
-var_dump($parmas_array);
 get_header(); ?>
 
 <main id="primary" class="site-main">
@@ -32,9 +26,7 @@ get_header(); ?>
     $page_title = $_GET['type'] !== null ? 'Repertuar imprez gościnnych' : 'Repertuar teatru';
     echo breadcrumb_block($page_title, $description); ?>
     <div class="container">
-      <?php
-
-      echo performance_render_template($month); ?>
+      <?php echo performance_render_template($month); ?>
     </div>
   </section>
 
