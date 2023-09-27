b3a3b0b4d5ffa37172b2b3b6b44efb438ca26190
tosha504 <tosha.rovniy@gmail.com>
2023-09-27 14:57:53 +0200

    content-people

--- template-parts/content-people.php
+++ template-parts/content-people.php
@@ -12,11 +12,18 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php
+	$curent_person_ID =  get_the_ID();
+	$performances = array(
+		'post_type' => 'performance',
+		'posts_per_page'   => -1,
+	);
+	$performances_query = new WP_Query($performances);
+
 	$people_field = get_field('people_field', get_the_ID());
 	$position = get_field('post', get_the_ID());
 	$brief_info = get_field('brief_info', get_the_ID());
 	$position_show = $position ? '<p class="position">' . $position . '</p>' : '';
-	$bierf_info_show = $brief_info ? $brief_info . '</br></br>' : ''; 
+	$bierf_info_show = $brief_info ? $brief_info . '</br></br>' : '';
 	echo do_shortcode('[search_nav]'); ?>
 	<section class="breadcrumbs">
 		<div class="container">
@@ -29,7 +36,7 @@
 				$excerpt =  has_excerpt() ? get_the_excerpt() : '';
 				echo '<div class="breadcrumbs__content_wrap">
 					<div>
-						<h1>' .	get_the_title() . '</h1>' . $position_show . 
+						<h1>' .	get_the_title() . '</h1>' . $position_show .
 					'</div>
 					<div>
 						<p>' . $bierf_info_show . $excerpt . ' </p>
@@ -43,24 +50,45 @@
 	<div class="container">
 		<div class="single-artist">
 			<div class="single-artist__fields">
-				<?php 
-					if($people_field) { 
-						foreach ($people_field as $key => $field) {
-							echo '<p>' . $field['title'] . '</p><br>';
-							foreach ($field['advantages'] as $key => $advantage) {
-								echo '<p><span>' . $advantage['advantages_title'] . '</span>' . $advantage['advantages_description'] . '</p>';
-							}
+				<?php
+				if ($people_field) {
+					foreach ($people_field as $key => $field) {
+						echo '<p>' . $field['title'] . '</p><br>';
+						foreach ($field['advantages'] as $key => $advantage) {
+							echo '<p><span>' . $advantage['advantages_title'] . '</span>' . $advantage['advantages_description'] . '</p>';
 						}
 					}
+				}
 				?>
 			</div>
 			<div class="single-artist__content">
-				<?php 
-					the_post_thumbnail();
-					the_content(); 
-				?>
+				<?php
+				the_post_thumbnail();
+				the_content();
+
+				if ($performances_query->have_posts()) { ?>
+					<div class="currently-playing">
+						<span class="currently-playing__title">Aktualnie gra w</span>
+						<ul>
+							<?php while ($performances_query->have_posts()) {
+								$performances_query->the_post(); ?>
+								<?php
+								$realists = get_field('realists',  get_the_ID());
+								foreach ($realists as $key => $realist) {
+									foreach ($realist['people'] as $key => $people) {
+										if ($curent_person_ID === $people['person']->ID) { ?>
+											<li><a href="<?php echo get_permalink(); ?>"><?php echo get_the_title(); ?></a></li>
+								<?php }
+									}
+								}
+								?>
+						<?php }
+						} ?>
+						</ul>
+					</div>
+					<?php wp_reset_query(); ?>
 			</div>
 		</div>
 	</div>
-	
+
 </article><!-- #post-<?php the_ID(); ?> -->
